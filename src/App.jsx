ceef1b214a6057c74bb8e49257dc56ef0f0d34f3
digvijay13-coder <digvijaysutar1004@gmail.com>
2025-12-09 18:22:18 +0530

    Initial project upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Components/Footer"
+import Hero from "./Components/Hero"
+import NavBar from "./Components/NavBar"
+
+function App() {
+  
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <Hero></Hero>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer()
+{
+    return(
+        <>
+        
+     <footer>
+        <div className="content">
+<a href="">Instagram</a>
+            </div>
+            
+            <div className="creator">
+<h5>Created By DIGVIJAY</h5>
+            </div>
+     </footer>
+               
+        </>
+    )
+
+}
